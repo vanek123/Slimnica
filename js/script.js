5b8b9e69ada2abe3a8a6883f9029b700eca5bb35
vanek123 <vanjaaleskevic@mail.ru>
2023-03-26 23:23:16 +0300

    login register for patients

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+/* hamburger  */
 hamburger = document.querySelector(".hamburger");
 hamburger.onclick = function() {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+
