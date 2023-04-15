17ee190dbff366db00ba1d5e81f391a2ea02526e
vanek123 <vanjaaleskevic@mail.ru>
2023-04-15 13:59:33 +0300

    doctor login php script done

--- arsts_login.php
+++ arsts_login.php
@@ -1,55 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/arsts_login.css">
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <title>Login</title>
-    <script defer src="js/admin_login.js"></script>
-</head>
-<body>
+<?php
+
+    session_start();
+    require 'connection.php';
+
+    if(isset($_POST['submit'])) 
+    {
+        $epasts = $_POST['email'];
+        $parole = $_POST['password'];
+
+        $SQL_stmt = "SELECT * FROM arsti WHERE epasts = '".$epasts."' AND parole = '".$parole."' ";
+
+        $result = $DBconnection->query($SQL_stmt);
+        $row = $result->fetch();
+
+        if (empty($epasts) || empty($parole)) {
+            header("location: arsts_login.php?activity=login_empty");
+            exit();
+        }
+        elseif ($row) {
+                $_SESSION['arsts_id'] = $row['arsts_id'];
+                $_SESSION['epasts'] = $row['epasts'];
+                $_SESSION['parole'] = $row['parole'];
     
-    <div class="container">
-        <div class="forms">
-            <div class="form login">
-                <span class="title">Doctor Login</span>
-
-                <form action="#">
-                    <div class="input-field">
-                        <input type="text" placeholder="Enter your email" required>
-                        <i class="uil uil-envelope icon"></i>
-                    </div>
-                    <div class="input-field">
-                        <input type="password" class="password" placeholder="Enter your password" required>
-                        <i class="uil uil-lock icon"></i>
-                        <i class="uil uil-eye-slash showHidePw"></i>
-                    </div>
-
-                    <div class="checkbox-text">
-                        <div class="checkbox-content">
-                            <input type="checkbox" id="logCheck">
-                            <label for="logCheck" class="text">Remember me</label>
-                        </div>
-
-                        <a href="#" class="text">Forgot password?</a>
-                    </div>
-
-                    <div class="input-field button">
-                        <input type="button" value="Login Now">
-                    </div>
-                </form>
-                
-                <div class="login-signup">
-                    <span class="text">Not a member?
-                        <a href="#" class="text signup-link">Signup now</a>
-                    </span>
-                </div>
-            </div>
+            header('location: doctor_logged.html?activity=successful');
+            exit();
+            }    
+    
+            else {
+                header('location: arsts_login.php?activity=incorrect_user_login_credentials');
+                exit(); 
+            }
             
-        </div>
-    </div>
+        
+    
+        }
+        else
+        {
+            header('location: arsts_login.php?activity=username_or_password_not_set');
+            exit();
+        }
+    
 
-</body>
-</html>
+?>
